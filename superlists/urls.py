fb0a0031faad6dcdc62929edacbbeee703506dfd
Max Schleper <mx.schleper@gmail.com>
2024-06-04 17:08:47 +0200

    new url, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
-from lists.views import home_page
+from lists import views
 from django.urls import path
 
 urlpatterns = [
-    path('', home_page, name="home"),
+    path('', views.home_page, name="home"),
+    path('lists/the-only-list-in-the-world/', views.view_list, name="view_list"),
 ]
